21d49aac33e164ec7c1ee0cac5dd374249d1302f
Sam Moon <sammoon90@gmail.com>
2022-04-25 21:42:00 -0400

    calls2.0

--- seed.sql
+++ seed.sql
@@ -12,12 +12,12 @@ VALUES
  ;
 
 INSERT INTO 
-students (name, age, phone_number, address)
+students (name, age, phone_number, address, front_end, back_end)
 VALUES
-    ('Tiger Tesla', 38, 2025550180, '11 Vermont Court, New City, NY 10956'),
-    ('Tom Hertz', 25, 2025550150, '87 Bayport Street, West Deptford, NJ 08096'),
-    ('Michael Ferrari', 30, 2025550161, '230 Mayfield Street, Lansdale, PA 19446'),
-    ('Scottie Rivian', 28, 2025550158, '77 Forest Drive, Decatur, GA 30030'),
-    ('Victor Fisker', 20, 2025550186, '8640 NE. Sutor Road, New Albany, IN 47150'),
-    ('Onyinye Rimac', 27, 2025550131, '80 Thatcher Lane, Pottstown, PA 19464')
+    ('Tiger Tesla', 38, 2025550180, '11 Vermont Court, New City, NY 10956', 'satisfactory', 'needs work'),
+    ('Tom Hertz', 25, 2025550150, '87 Bayport Street, West Deptford, NJ 08096', 'needs work', 'acceptable'),
+    ('Michael Ferrari', 30, 2025550161, '230 Mayfield Street, Lansdale, PA 19446', 'acceptable', 'unacceptable'),
+    ('Scottie Rivian', 28, 2025550158, '77 Forest Drive, Decatur, GA 30030', 'unacceptable', 'acceptable'),
+    ('Victor Fisker', 20, 2025550186, '8640 NE. Sutor Road, New Albany, IN 47150', 'acceptable', 'satisfactory'),
+    ('Onyinye Rimac', 27, 2025550131, '80 Thatcher Lane, Pottstown, PA 19464', 'needs work', 'satisfactory')
 ;
